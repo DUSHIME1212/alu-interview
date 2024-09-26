a76cda4d74053c91f4889893aa263b162bf2e50c
DUSHIME1212 <mdonavan33@gmail.com>
2024-09-26 22:39:56 +0200

    Remove unnecessary lines and whitespace in minOperations function.

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -9,11 +9,9 @@ exactly n H characters in the file.
 def minOperations(n):
     if n < 2:
         return 0
-
     current_num_of_h = 1
     copied = 0
     num_of_operations = 0
-
     while current_num_of_h < n:
         if n % current_num_of_h == 0:
             copied = current_num_of_h
